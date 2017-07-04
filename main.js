6586e65818887714892b1ef327285291c857974f
n3uron3 <Easierkreazper@gmail.com>
2017-07-04 15:47:14 -0600

    ES2015 syntax implemented

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 (() => {
 	let box = document.getElementById('box')
-	function moveRand(){
-		box.style.transform = `translate(${Math.random()*500}px, ${Math.random()*500}px)`
-	}
+	let moveRand = () => box.style.transform = `translate(${Math.random()*500}px, ${Math.random()*500}px)`
 	box.addEventListener('mouseover', () => moveRand() )
 	box.addEventListener('dblclick', () => alert('You won!'))
 })();
