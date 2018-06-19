0a1c5a50e280c7538c5db6563f0fa7165455c98d
Diego Jardon <diegojardonc@gmail.com>
2018-06-19 14:02:58 -0500

    Refactored

--- main.js
+++ main.js
@@ -1,55 +1,66 @@
 const box = document.getElementById('box');
-const gamesWonCounter = document.querySelector("#gamesWon");
-const difficultySelect = document.getElementById("difficulty");
-const unit = "px";
+const gamesWonCounter = document.getElementById('gamesWon')
+const difficultySelect = document.getElementById('difficulty');
+const unit = 'px';
+
+let touchMode = false;
 let gamesWon = 0;
 
+// Resets box initial state
+function reset () {
+	box.style.left = '0px'
+	box.style.top = '0px'
+	box.style.backgroundColor = 'black'
+}
+
+function moveRandom (el) {
+
+	let maximumMovementRange = {
+		get left () {
+			let width = window.getComputedStyle(el).width.substring(0, window.getComputedStyle(el).width.length-2);
+			let left = el.offsetLeft-el.style.width;
+			let right = window.innerWidth-left;
+			return (left <= right) ? left : right;
+		},
+		get top () {
+			let height = window.getComputedStyle(el).height.substring(0, window.getComputedStyle(el).height.length-2);
+			let top = el.offsetTop-height;
+			let bottom = window.innerHeight-top;
+			return (top <= bottom) ? top : bottom;
+		}
+	};
+
+	let direction = (Math.floor(Math.random*10) >= 5) ? 1 : -1;
+
+	el.style.top = `${Math.floor(Math.random()*maximumMovementRange.top)*direction+unit}`;
+	el.style.left = `${Math.floor(Math.random()*maximumMovementRange.left)*direction+unit}`;
+	el.style.backgroundColor = `hsl(${Math.floor(Math.random()*360)}, 100%, 50%)`;
+}
+
 window.onload = () => {
-
-	box.style.top = 0; box.style.left = 0;
-
-	let moveRand = () => {
-		
-		let maximumMovementRange = {
-			get left () {
-				let width = window.getComputedStyle(box).width.substring(0, window.getComputedStyle(box).width.length-2);
-				let left = box.offsetLeft-box.style.width;
-				let right = window.innerWidth-left;
-				return (left <= right) ? left : right;
-			},
-			get top () {
-				let height = window.getComputedStyle(box).height.substring(0, window.getComputedStyle(box).height.length-2);
-				let top = box.offsetTop-height;
-				let bottom = window.innerHeight-top;
-				return (top <= bottom) ? top : bottom;
-			}
-		};
-
-		let direction = (Math.floor(Math.random()*10) >= 5) ? 1 : -1;
-		box.style.top = `${Math.floor(Math.random()*maximumMovementRange.top)*direction+unit}`;
-		box.style.left = `${Math.floor(Math.random()*maximumMovementRange.left)*direction+unit}`;
-		box.style.backgroundColor = `hsl(${Math.floor(Math.random()*360)}, 100%, 50%)`;
+	// On touch devices, just start moving randomly
+	if (touchMode) {
+		let pid = setInterval(moveRand, (0.75/4)*1000)
 	}
 
-	difficultySelect.addEventListener("change", function (e) {
+	difficultySelect.addEventListener('change', function (e) {
 		box.style.transition = `all ${0.75/parseInt(e.target.value)}s linear`;
-	});
-	box.addEventListener('mouseover', moveRand)
+	})
+
+	box.addEventListener('mouseover', () => {
+		moveRandom(box)
+	})
+
 	box.addEventListener('click', () => {
+		// Win Screen
 		alert("You've won!");
 
-		// Reset box initial position
-		box.style.top = 0+"px";
-		box.style.left = 0+"px";
-		box.style.backgroundColor = "black";
-
+		// Reset box state and update win counter
+		reset()
 		gamesWon++;
-		gamesWonCounter.innerHTML = gamesWon;
+		gamesWonCounter.innerText = gamesWon;
 	});
+
 }
 
-window.onresize = function () {
-	box.style.left = 0+"px";
-	box.style.top = 0+"px";
-	box.style.backgroundColor = "black";
-}
+window.onresize = reset
